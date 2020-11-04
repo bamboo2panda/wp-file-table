1a3334c862882b0a41ea19d601702a22257319f1
Pavel <bamboo2panda@gmail.com>
2020-11-04 13:23:22 +0300

    Composer init commit

---
+++ wp-file-table.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: WP File Table
+Plugin URI: https://github.com/bamboo2panda/wp-file-table
+Description: Wordpress plugin for creating managable tables of files. .
+Version: 1.0
+Author: bamboo2panda
+Author URI: https://github.com/bamboo2panda/
+License: GPL2
+*/
+
+defined('ABSPATH') or die ('No direct access to file!');
