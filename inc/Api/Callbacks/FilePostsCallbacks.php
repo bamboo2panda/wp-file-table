25c7fbb9c4ff85b51061732a02bd7f24c1677dc5
Pavel <bamboo2panda@gmail.com>
2020-11-23 10:52:59 +0300

    Add optional links to admin panel

--- inc/Api/Callbacks/FilePostsCallbacks.php
+++ inc/Api/Callbacks/FilePostsCallbacks.php
@@ -23,7 +23,18 @@ class FilePostsCallbacks extends BaseController
             return false;
         }
         $meta = [
+            'attachment_name' => $_POST['attachment_name'],
             'attachment_link' => $_POST['attachment_link'],
+            'attachment_style' => $_POST['attachment_style'],
+
+            'attachment_name_1' => $_POST['attachment_name_1'],
+            'attachment_link_1' => $_POST['attachment_link_1'],
+            'attachment_style_1' => $_POST['attachment_style_1'],
+
+            'attachment_name_2' => $_POST['attachment_name_2'],
+            'attachment_link_2' => $_POST['attachment_link_2'],
+            'attachment_style_2' => $_POST['attachment_style_2'],
+
         ];
 
         update_post_meta($post_id, '_file_post_meta_key', $meta);
@@ -31,30 +42,96 @@ class FilePostsCallbacks extends BaseController
 
     public function attachFile()
     {
-        echo '<a href="/wp-admin/media-upload.php?post_id=271&amp;type=file&amp;TB_iframe=1&amp;width=753&amp;height=270" id="set-post-thumbnail" class="thickbox">Установить изображение записи</a>';
-        $this->inputAttachmentLink();
+        $links = array(
+            array(
+                'name'=>'attachment_name',
+                'name_label'=>'Название ссылки',
+                'link'=>'attachment_link',
+                'link_label'=>'Основная ссылка',
+                'style'=>'attachment_style',
+            ),
+            array(
+                'name'=>'attachment_name_1',
+                'name_label'=>'Название ссылки 1',
+                'link'=>'attachment_link_1',
+                'link_label'=>'Дополнительная ссылка 1',
+                'style'=>'attachment_style_1',
+            ),
+            array(
+                'name'=>'attachment_name_2',
+                'name_label'=>'Название ссылки 2',
+                'link'=>'attachment_link_2',
+                'link_label'=>'Дополнительная ссылка 2',
+                'style'=>'attachment_style_2',
+            ),
+        );
+        foreach ($links as $link){
+            $this->inputAttachmentName($link);
+            $this->inputAttachmentLink($link);
+            $this->inputAttachmentStyle($link);
+        }
     }
 
-    private function inputAttachmentLink()
+    private function inputAttachmentName(array $link)
     {
-        $file_link = $this->getAttachmentLink();
+        echo '<p>';
+        $this->labelAttachmentName($link['name_label'], $link['name']);
+        $link_name = $this->getPostMetaParam($link['name']);
+
         echo '
-            <p>
-                <label for="attachment_link">Ссылка на файл</label>
-                <input 
-                    type="text" 
-                    value="'.$file_link.'" 
-                    name="attachment_link" 
-                    id="attachment_link"
-                >
-            </p>
-        ';
+            <input 
+                type="text" 
+                value="'.$link_name.'" 
+                name="'.$link['name'].'" 
+                id="'.$link['name'].'"
+            >';
+        echo '</p>';
     }
 
-    private function getAttachmentLink()
+    private function inputAttachmentLink(array $link)
+    {
+        echo '<p>';
+        $this->labelAttachmentLink($link['link_label'], $link['link']);
+        $attachment_link = $this->getPostMetaParam($link['link']);
+        echo '
+            <input 
+                type="text" 
+                value="'.$attachment_link.'" 
+                name="'.$link['link'].'" 
+                id="'.$link['link'].'"
+            >';
+        echo '</p>';
+    }
+
+
+    private function inputAttachmentStyle(array $link)
+    {
+        $attachment_style = $this->getPostMetaParam($link['style']);
+
+        echo '
+            <select name="'.$link['style'].'" id="'.$link['style'].'">
+                <option value="blue" '. ($attachment_style == "blue" ? "selected" : " ") .'>Синяя кнопка</option>
+                <option value="green" '. ($attachment_style == "green" ? "selected" : " ")  .'>Зелёная кнопка</option>
+                <option value="red" '. ($attachment_style == "red" ? "selected" : " ") .'>Красная кнопка</option>
+                <option value="simple" '. ($attachment_style == "simple" ? "selected" : " ").'>Простая ссылка</option>
+            </select>';
+    }
+
+    private function labelAttachmentLink($label, $link)
+    {
+        echo '<p><label for="'.$link.'">'.$label.'</label>';
+    }
+
+    private function labelAttachmentName($label, $link_name)
+    {
+        echo '<label for="'.$link_name.'">'.$label.'</label>';
+    }
+
+    private function getPostMetaParam($name)
     {
         global $post;
         $meta = get_post_meta($post->ID, '_file_post_meta_key', true);
-        return is_array($meta) && key_exists('attachment_link', $meta) ? $meta['attachment_link'] : '';
+        return is_array($meta) && key_exists($name, $meta) ? $meta[$name] : '';
     }
+
 }
